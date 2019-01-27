c1e6c9a458f4e966c90a6bfa82bffba3542796c1
jbao161 <jbao161@gmail.com>
2019-01-26 21:48:40 -0700

    fix syntax errors

--- vod_nospoil.js
+++ vod_nospoil.js
@@ -1,7 +1,6 @@
 //https://alchemytheory.blogspot.com/2019/01/watch-youtube-spoiler-free.html
-
 var youtube_regex = 
-/^https:\/\/www\.youtube\.com\/watch\?v=([^&]*)(?:.*(?:(?=(?:&t=(?:(?:(?:[01]?\d|2[0-3])h)?(?:[0-5]?\d)m)?(?:[0-5]?\d)s))))?(&t=(?:(?:([01]?\d|2[0-3])h)?([0-5]?\d)m)?([0-5]?\d)s)?/
+/https:\/\/www\.youtube\.com\/watch\?v=([^&]*)(?:.*(?:(?=(?:&t=(?:(?:(?:[01]?\d|2[0-3])h)?(?:[0-5]?\d)m)?(?:[0-5]?\d)s))))?(&t=(?:(?:([01]?\d|2[0-3])h)?([0-5]?\d)m)?([0-5]?\d)s)?/
 ;
 var twitch_regex = /^https:\/\/www.twitch.tv\/videos\/([^?]*)(?:.*(?:(?=(?:&t=(?:(?:(?:[01]?\d|2[0-3])h)?(?:[0-5]?\d)m)?(?:[0-5]?\d)s))))?(?:\?t=((?:(?:([01]?\d|2[0-3])h)?([0-5]?\d)m)?([0-5]?\d)s))?/;
 
@@ -25,10 +24,10 @@ if (youtube_match!==null){
 var twitch_match = twitch_regex.exec(source_url);
 twitch_match;
 if (twitch_match!==null){
-        var new_url_twitch = source_url.replace(twitch_regex,
-        "https://player.twitch.tv/?video=v$1&controls=false";);
+        var new_url_twitch;
         if (twitch_match[2]!=null){
-            new_url_twitch = new_url_twitch.replace(twitch_regex,
-        "https://player.twitch.tv/?video=v$1&controls=false&time=$2";
-        }
+            new_url_twitch = source_url.replace(twitch_regex,
+        "https://player.twitch.tv/?video=v$1&controls=false&time=$2");
+        } else {new_url_twitch= source_url.replace(twitch_regex,
+        "https://player.twitch.tv/?video=v$1&controls=false");}
         window.location.href = new_url_twitch;}
